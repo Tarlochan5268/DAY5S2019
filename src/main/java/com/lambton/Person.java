e19dae3ad0006b71b472406d3f1b2bb04c52550a
Tarlochan5268 <brotherstarlochan5268@gmail.com>
2019-05-28 18:36:46 -0400

    [Tarlochan] Created Person Class

--- src/main/java/com/lambton/Person.java
+++ src/main/java/com/lambton/Person.java
@@ -1,6 +1,74 @@
 package com.lambton;
 
+import java.util.Date;
+
 public class Person
 {
-    
+    private int id;
+    private String firstname;
+    private String lastname;
+    private char gender;
+    private Date birthdate;
+    private String email;
+
+    //blank constructor
+    public Person() {
+    }
+
+    public Person(int id, String firstname, String lastname, char gender, Date birthdate, String email) {
+        this.id = id;
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.gender = gender;
+        this.birthdate = birthdate;
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public char getGender() {
+        return gender;
+    }
+
+    public void setGender(char gender) {
+        this.gender = gender;
+    }
+
+    public Date getBirthdate() {
+        return birthdate;
+    }
+
+    public void setBirthdate(Date birthdate) {
+        this.birthdate = birthdate;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
